b03c74a4f9d95d5a33cf39d2dd76072006b3eb64
Errorprone85 <aaron.gedmin@gmail.com>
2026-04-19 11:36:34 -0400

    Add: dynamicArray - submission-3

---
+++ Data Structures & Algorithms/dynamicArray/submission-3.cs
@@ -0,0 +1,49 @@
+public class DynamicArray {
+    private int[] arr;
+    private int size;
+    
+    public DynamicArray(int capacity) {
+        arr = new int[capacity];
+        size = 0;
+    }
+
+    public int Get(int i)
+    {
+        var val = arr[i];
+
+        return val;
+    }
+
+    public void Set(int i, int n) {
+        arr[i] = n;
+    }
+
+    public void PushBack(int n) {
+        if (size >=  arr.Length)
+            Resize();
+        
+        arr[size] = n;
+        size++;
+    }
+
+    public int PopBack() {
+        var val = arr[--size];
+
+        return val;
+    }
+
+    private void Resize() {
+        var newArr = new int[arr.Length * 2];
+        Array.Copy(arr, newArr, arr.Length);
+        arr = newArr;
+    }
+
+    public int GetSize()
+    {
+        return size;
+    }
+
+    public int GetCapacity() {
+        return arr.Length;
+    }
+}
