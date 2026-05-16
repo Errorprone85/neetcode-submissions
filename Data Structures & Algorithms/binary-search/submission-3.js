800458fccf2b7e6939225debded30917733d0595
Errorprone85 <aaron.gedmin@gmail.com>
2026-05-16 10:12:25 -0400

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        if (nums.length === 1 && nums[0] === target) return 0;
+
+        let leftIndex = 0;
+        let rightIndex = nums.length - 1;
+
+        while (leftIndex <= rightIndex) {
+            const midpoint = Math.floor(leftIndex + (rightIndex - leftIndex) / 2);
+
+            if (nums[midpoint] === target) return midpoint;
+
+            if (target >= nums[midpoint]) {
+                leftIndex = midpoint + 1;
+            } else {
+                rightIndex = midpoint - 1;
+            }
+        }
+
+        return -1;
+    }
+}
